230ea323bf611797b9dbfb173e9cb0e4c032fc2d
feldan <zhxllongge@outlook.com>
2022-06-10 15:22:01 +0800

    fix:shell

--- Linux课程设计/myfile.sh
+++
@@ -1,7 +0,0 @@
-cat myfile.sh
-echo 1
-echo 2
-echo 3
-sleep 20
-echo 4
-echo 5
---
+++ Linux课程设计/shell/myfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# file        :myfile.sh
+# Author      :feldan
+# url https://blog.feldan.top
+# Date        :2022/06/10 14:24:19
+cat myfile.sh
+echo 1
+echo 2
+echo 3
+sleep 20
+echo 4
+echo 5
